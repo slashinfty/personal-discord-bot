0ac1bd8a0aa18fc77f6623db27da91e72f764ca1
Matt Braddock <matt.braddock@gmail.com>
2022-03-08 21:33:41 -0500

    clarifications

--- index.js
+++ index.js
@@ -4,15 +4,22 @@ import * as Command from './commands.js';
 
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
+const reminders = [];
+
+const information = [];
+
 client.once('ready', async () => {
 
+    setInterval(() => { //will use process.env.DISCORD_CHANNEL
+
+    }, 60000);
 });
 
 client.on('interactionCreate', async interaction => {
     if (!interaction.isCommand()) return;
 
     try {
-        Command[interaction.commandName.replace(/-([a-z])/g, str => str[1].toUpperCase())](interaction, db, manager, role);
+        Command[interaction.commandName.replace(/-([a-z])/g, str => str[1].toUpperCase())](interaction, /reminder/.test(interaction.commandName) ? reminders : information);
     } catch (e) {
         console.error(e);
         await interaction.reply({
