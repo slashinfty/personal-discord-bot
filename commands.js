45e03c5fd91d3889dfb894dcf509c4718eebdc71
Matt Braddock <matt.braddock@gmail.com>
2022-03-09 08:44:14 -0500

    updates

--- commands.js
+++ commands.js
@@ -1,15 +1,61 @@
 import { SlashCommandBuilder } from '@discordjs/builders';
 
+import { reminders, information } from './index.js';
+
 export const commandBuilders = [
     new SlashCommandBuilder()
-        .setName('new-reminder')
-        .setDescription('Create a new reminder')
+        .setName('repeat-reminder')
+        .setDescription('Create a reminder that repeats')
         .addStringOption(option => option
             .setName('content')
             .setDescription('Content of the reminder')
             .setRequired(true))
+        .addStringOption(option => option
+            .setName('days')
+            .setDescription('Comma separated (Sun=0)')
+            .setRequired(true))
+        .addStringOption(option => option
+            .setName('time')
+            .setDescription('HH:MM (24hr time)')
+            .setRequired(true)),
+    new SlashCommandBuilder()
+        .setName('single-reminder')
+        .setDescription('Create a one-time reminder')
+        .addStringOption(option => option
+            .setName('content')
+            .setDescription('Content of the reminder')
+            .setRequired(true))
+        .addStringOption(option => option
+            .setName('date')
+            .setDescription('MM/DD (optional)'))
+        .addStringOption(option => option
+            .setName('time')
+            .setDescription('HH:MM (24hr time)')
+            .setRequired(true)),
+    new SlashCommandBuilder()
+        .setName('query')
+        .setDescription('Search for saved information')
+        .addStringOption(option => option
+            .setName('search')
+            .setDescription('Word/phrase to search for')
+            .setRequired(true)),
+    new SlashCommandBuilder()
+        .setName('save-info')
+        .setDescription('Information to save')
+        .addStringOption(option => option
+            .setName('search')
+            .setDescription('Information to save')
+            .setRequired(true))
 ];
 
-export const newReminder = async (interaction) => {
+export const repeatReminder = async (interaction) => {
+
+}
+
+export const singleReminder = async (interaction) => {
+
+}
+
+export const query = async (interaction) => {
 
 }
--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 import { Client, Intents } from 'discord.js';
+import fs from 'fs';
 import 'dotenv/config';
 import * as Command from './commands.js';
 
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
-const reminders = [];
+// check if reminders and information files exist, and create them if necessary
 
-const information = [];
+export const reminders = JSON.parse(fs.readFileSync(new URL('./reminders.json', import.meta.url)));
+
+export const information = JSON.parse(fs.readFileSync(new URL('./information.json', import.meta.url)));
 
 client.once('ready', async () => {
 
@@ -19,7 +22,7 @@ client.on('interactionCreate', async interaction => {
     if (!interaction.isCommand()) return;
 
     try {
-        Command[interaction.commandName.replace(/-([a-z])/g, str => str[1].toUpperCase())](interaction, /reminder/.test(interaction.commandName) ? reminders : information);
+        Command[interaction.commandName.replace(/-([a-z])/g, str => str[1].toUpperCase())](interaction);
     } catch (e) {
         console.error(e);
         await interaction.reply({
